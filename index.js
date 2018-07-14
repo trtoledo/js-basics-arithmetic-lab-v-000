fafcdf9a9edd440735b154896529542e1d573175
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-14 21:55:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-let newID;
-newID = ++oldID = 1000000000;
+let newID = 1000000000;
+newID = ++oldID ;
