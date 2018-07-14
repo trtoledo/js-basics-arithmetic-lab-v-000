8a5b6ee71271835271e806b3676a0745d8e516a5
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-14 22:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const newID = oldID + 1000000000;
 
 const ageIsValid = Number.isInteger(currentAge);
 
-const randomNumber = Math.random() * 20
+const randomNumber = Math.random() * 20;
 
-const randomInteger = Math.floor(randomNumber)
+const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = Number.parseInt(randomInteger)
